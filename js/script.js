aae81a8b30c6ed9188b71c2c71137f4fcf8339a5
bittoMazza <robertomazza99@gmail.com>
2022-06-13 15:34:53 +0200

    add comments

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-
+//Prendiamo in input dall'utente nome,cognome e colore
 const userName = prompt('Inserisci il tuo nome');
 const userLastName = prompt('Inserisci il tuo cognome');
 const userFavColour = prompt('Inserisci il tuo colore preferito');
 
+//Scriviamo i valori inseriti in precedenza dall'utente all'interno dei tag con i rispettivi id
 document.getElementById('user-name').innerHTML = userName;
 document.getElementById('user-last-name').innerHTML = userLastName;
 document.getElementById('user-fav-colour').innerHTML = userFavColour;
 document.getElementById('user-password').innerHTML = userName + userLastName + userFavColour+"22" 
 
+//Scriviamo in console la password ottenuta
 console.log(userName+userLastName+userFavColour+"22")
