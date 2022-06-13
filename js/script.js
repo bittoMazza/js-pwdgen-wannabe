8f3d8a9de6df4a2ea2d2cd143b509e50c461639d
bittoMazza <robertomazza99@gmail.com>
2022-06-13 14:32:39 +0200

    creation repo,completed exercise

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+let userName = prompt('Inserisci il tuo nome');
+let userSurname = prompt('Inserisci il tuo cognome');
+let userFavColour = prompt('Inserisci il tuo colore preferito');
+
+document.getElementById('user-password').innerHTML = userName + userSurname + userFavColour+"22" 
+
+console.log(userName+userSurname+userFavColour+"22")
