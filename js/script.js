7e9db7d4c8b023ca37ff194d076199310e41b5a0
bittoMazza <robertomazza99@gmail.com>
2022-06-13 14:58:37 +0200

    add some output

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 
-let userName = prompt('Inserisci il tuo nome');
-let userSurname = prompt('Inserisci il tuo cognome');
-let userFavColour = prompt('Inserisci il tuo colore preferito');
+const userName = prompt('Inserisci il tuo nome');
+const userLastName = prompt('Inserisci il tuo cognome');
+const userFavColour = prompt('Inserisci il tuo colore preferito');
 
-document.getElementById('user-password').innerHTML = userName + userSurname + userFavColour+"22" 
+document.getElementById('user-name').innerHTML = userName;
+document.getElementById('user-last-name').innerHTML = userLastName;
+document.getElementById('user-fav-colour').innerHTML = userFavColour;
+document.getElementById('user-password').innerHTML = userName + userLastName + userFavColour+"22" 
 
-console.log(userName+userSurname+userFavColour+"22")
+console.log(userName+userLastName+userFavColour+"22")
