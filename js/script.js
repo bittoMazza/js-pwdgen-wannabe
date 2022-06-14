21072dc4435f768f7c6478bc50b5adacd01f59f3
bittoMazza <robertomazza99@gmail.com>
2022-06-14 09:56:03 +0200

    create one variable for userPassword

--- js/script.js
+++ js/script.js
@@ -2,12 +2,14 @@
 const userName = prompt('Inserisci il tuo nome');
 const userLastName = prompt('Inserisci il tuo cognome');
 const userFavColour = prompt('Inserisci il tuo colore preferito');
+let userPassword;
+userPassword= userName + userLastName + userFavColour + "22";
 
 //Scriviamo i valori inseriti in precedenza dall'utente all'interno dei tag con i rispettivi id
 document.getElementById('user-name').innerHTML = userName;
 document.getElementById('user-last-name').innerHTML = userLastName;
 document.getElementById('user-fav-colour').innerHTML = userFavColour;
-document.getElementById('user-password').innerHTML = userName + userLastName + userFavColour+"22" 
+document.getElementById('user-password').innerHTML = userPassword;
 
 //Scriviamo in console la password ottenuta
-console.log(userName+userLastName+userFavColour+"22")
+console.log(userPassword)
